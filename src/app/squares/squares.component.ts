93b91805824dd896567ff903b88215a6abc34280
Illes Miklos <illes.miklos95@gmail.com>
2020-09-28 11:17:27 +0200

    remove unused code

--- src/app/squares/squares.component.ts
+++ src/app/squares/squares.component.ts
@@ -7,16 +7,8 @@ import { Square } from '../square';
   styleUrls: ['./squares.component.css'],
 })
 export class SquaresComponent implements OnInit {
-  public squares: Square[][]; /*= [
-    [{x:0, y:0, hasKnight:false},{x:1, y:0, hasKnight:false},{x:2, y:0, hasKnight:false},{x:3, y:0, hasKnight:false},{x:4, y:0, hasKnight:false},{x:5, y:0, hasKnight:false},{x:6, y:0, hasKnight:false},{x:7, y:0, hasKnight:false}],
-    [{x:0, y:1, hasKnight:false},{x:1, y:1, hasKnight:false},{x:2, y:1, hasKnight:false},{x:3, y:1, hasKnight:false},{x:4, y:1, hasKnight:false},{x:5, y:1, hasKnight:false},{x:6, y:1, hasKnight:false},{x:7, y:1, hasKnight:false}],
-    [{x:0, y:2, hasKnight:false},{x:1, y:2, hasKnight:false},{x:2, y:2, hasKnight:false},{x:3, y:2, hasKnight:false},{x:4, y:2, hasKnight:false},{x:5, y:2, hasKnight:false},{x:6, y:2, hasKnight:false},{x:7, y:2, hasKnight:false}],
-    [{x:0, y:3, hasKnight:false},{x:1, y:3, hasKnight:false},{x:2, y:3, hasKnight:false},{x:3, y:3, hasKnight:false},{x:4, y:3, hasKnight:false},{x:5, y:3, hasKnight:false},{x:6, y:3, hasKnight:false},{x:7, y:3, hasKnight:false}],
-    [{x:0, y:4, hasKnight:false},{x:1, y:4, hasKnight:false},{x:2, y:4, hasKnight:false},{x:3, y:4, hasKnight:false},{x:4, y:4, hasKnight:false},{x:5, y:4, hasKnight:false},{x:6, y:4, hasKnight:false},{x:7, y:4, hasKnight:false}],
-    [{x:0, y:5, hasKnight:false},{x:1, y:5, hasKnight:false},{x:2, y:5, hasKnight:false},{x:3, y:5, hasKnight:false},{x:4, y:5, hasKnight:false},{x:5, y:5, hasKnight:false},{x:6, y:5, hasKnight:false},{x:7, y:5, hasKnight:false}],
-    [{x:0, y:6, hasKnight:false},{x:1, y:6, hasKnight:false},{x:2, y:6, hasKnight:false},{x:3, y:6, hasKnight:false},{x:4, y:6, hasKnight:false},{x:5, y:6, hasKnight:false},{x:6, y:6, hasKnight:false},{x:7, y:6, hasKnight:false}],
-    [{x:0, y:7, hasKnight:false},{x:1, y:7, hasKnight:false},{x:2, y:7, hasKnight:false},{x:3, y:7, hasKnight:false},{x:4, y:7, hasKnight:false},{x:5, y:7, hasKnight:false},{x:6, y:7, hasKnight:false},{x:7, y:7, hasKnight:false}]
-  ];*/
+  public squares: Square[][];
+
   constructor() {
     this.squares = [];
     for (let i = 0; i < 8; i++) {
@@ -35,6 +27,5 @@ export class SquaresComponent implements OnInit {
       element.forEach((e) => (e.hasKnight = false))
     );
     this.squares[cell.x][cell.y].hasKnight = true;
-    console.log(this.squares);
   }
 }
